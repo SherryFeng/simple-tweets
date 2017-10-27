d453cb8c73fbbefe61335fbecf78e8792ce92ac5
sherry feng <sherryfeng12@gmail.com>
2017-10-27 15:34:45 -0400

    first commit

---
+++ twitter.py
@@ -0,0 +1,12 @@
+import tweepy
+from secrets import consumer_key, consumer_secret, access_token, access_token_secret
+ 
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+ 
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+ 
+# Sample method, used to update a status
+print api.me()
