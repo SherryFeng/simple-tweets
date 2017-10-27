3e6cd5cbe1e1c71431377dc3dcac9d5c6108f9a8
Jiehan Zheng <zheng@jiehan.org>
2017-10-27 15:52:54 -0400

    more random changes

--- twitter.py
+++ twitter.py
@@ -2,11 +2,12 @@ import tweepy
 from secrets import consumer_key, consumer_secret, access_token, access_token_secret
 
 # OAuth process, using the keys and tokens
+# hello 2
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 # Creation of the actual interface, using authentication
 api = tweepy.API(auth)
- 
+
 # Sample method, used to print my info
 print api.me()
